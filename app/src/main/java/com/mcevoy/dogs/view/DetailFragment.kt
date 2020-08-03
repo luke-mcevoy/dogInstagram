fa6e0a9477f96bc86b2bbf329edcc8735d64df3d
lmcevoy <lmcevoy@stevens.edu>
2020-08-03 18:09:49 -0400

    navigationUI, actionBar, button click listeners + actions

--- app/src/main/java/com/mcevoy/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/mcevoy/dogs/view/DetailFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.mcevoy.dogs.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 
 class DetailFragment : Fragment() {
@@ -16,5 +18,13 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonList.setOnClickListener {
+            val action = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
