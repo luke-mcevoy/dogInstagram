bd756828bffb24eaa7967f98978b0137553beeaf
lmcevoy <lmcevoy@stevens.edu>
2020-08-04 18:14:28 -0400

    Retrieving + storing data to local database

--- app/src/main/java/com/mcevoy/dogs/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/mcevoy/dogs/viewmodel/ListViewModel.kt
@@ -1,11 +1,13 @@
 package com.mcevoy.dogs.viewmodel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mcevoy.dogs.model.DogBreed
 import com.mcevoy.dogs.model.DogDatabase
 import com.mcevoy.dogs.model.DogsAPIService
+import com.mcevoy.dogs.util.SharedPreferencesHelper
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
@@ -13,6 +15,9 @@ import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.launch
 
 class ListViewModel(application: Application): BaseViewModel(application) {
+
+    private var prefHelper = SharedPreferencesHelper(getApplication())
+    private var refreshTime = 5 * 60 * 1000 * 1000 * 1000L  // 5 minutes in nanoseconds
 
     private val dogsService = DogsAPIService()
     private val disposable = CompositeDisposable()
@@ -22,7 +27,25 @@ class ListViewModel(application: Application): BaseViewModel(application) {
     val loading = MutableLiveData<Boolean>()
 
     fun refresh() {
+        val updateTime = prefHelper.getUpdateTime()
+        if (updateTime != null && updateTime != 0L && System.nanoTime() - updateTime < refreshTime) {
+            fetchFromDatabase()
+        } else {
+            fetchFromRemote()
+        }
+    }
+
+    fun refreshBypassCache() {
         fetchFromRemote()
+    }
+
+    private fun fetchFromDatabase() {
+        loading.value = true
+        launch {
+            val dogs = DogDatabase(getApplication()).dogDao().getAllDogs()
+            dogsRetrieved(dogs)
+            Toast.makeText(getApplication(), "Dogs retrieved from database", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun fetchFromRemote() {
@@ -34,6 +57,7 @@ class ListViewModel(application: Application): BaseViewModel(application) {
                 .subscribeWith(object: DisposableSingleObserver<List<DogBreed>>() {
                     override fun onSuccess(dogList: List<DogBreed>) {
                         storeDogsLocally(dogList)
+                        Toast.makeText(getApplication(), "Dogs retrieved from endpoint", Toast.LENGTH_SHORT).show()
                     }
 
                     override fun onError(e: Throwable) {
@@ -64,6 +88,7 @@ class ListViewModel(application: Application): BaseViewModel(application) {
             }
             dogsRetrieved(list)
         }
+        prefHelper.saveUpdateTime(System.nanoTime())
     }
 
     override fun onCleared() {
