5b47cda6e2867d0766799b11800e1ce2dc2adf60
lmcevoy <lmcevoy@stevens.edu>
2020-08-04 14:41:45 -0400

    Recieving remote data from API, ListViewModel RxJava Observable with GET return Single

--- app/src/main/java/com/mcevoy/dogs/view/ListFragment.kt
+++ app/src/main/java/com/mcevoy/dogs/view/ListFragment.kt
@@ -37,6 +37,14 @@ class ListFragment : Fragment() {
             adapter = dogsListAdapter
         }
 
+        refreshLayout.setOnRefreshListener {
+            dogsList.visibility = View.GONE
+            listError.visibility = View.GONE
+            loadingView.visibility = View.VISIBLE
+            viewModel.refresh()
+            refreshLayout.isRefreshing = false
+        }
+
         observeViewModel()
     }
 
