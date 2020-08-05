b03d9c8ce288a65a139e216909067fdfbd2d767f
lmcevoy <lmcevoy@stevens.edu>
2020-08-05 16:10:26 -0400

    Implementing Palette for Detail Page

--- app/src/main/java/com/mcevoy/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/mcevoy/dogs/view/DetailFragment.kt
@@ -1,5 +1,7 @@
 package com.mcevoy.dogs.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +10,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.mcevoy.dogs.R
 import com.mcevoy.dogs.databinding.FragmentDetailBinding
+import com.mcevoy.dogs.model.DogPalette
 import com.mcevoy.dogs.util.getProgressDrawable
 import com.mcevoy.dogs.util.loadImage
 import com.mcevoy.dogs.viewmodel.DetailViewModel
@@ -48,7 +55,31 @@ class DetailFragment : Fragment() {
         viewModel.dogLiveData.observe(this, Observer { dog ->
             dog?.let {
                 dataBinding.dog = dog
+
+                it.imageUrl?.let { image ->
+                    setupBackgroundColor(image)
+                }
             }
         })
     }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object: CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {}
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate{palette ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?: 0
+                            val myPalette = DogPalette(intColor)
+                            dataBinding.palette = myPalette
+                        }
+                }
+
+            })
+    }
+
 }
